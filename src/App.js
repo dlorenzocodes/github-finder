cb4d08492601c7320b6b4d4fbfda6a90da1eca5e
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-01-26 09:42:22 -0600

    clean up fetch users

--- src/App.js
+++ src/App.js
@@ -4,25 +4,28 @@ import Footer from './components/layout/Footer'
 import Home from'./components/pages/Home'
 import About from'./components/pages/About'
 import NotFound from'./components/pages/NotFound'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-          <NavBar/>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+            <NavBar/>
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route path='/about' element={<About />}/>
-              <Route path='/notfound' element={<NotFound />}/>
-              <Route path='/*' element={<NotFound />}/>
-            </Routes>
-          </main>
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='/notfound' element={<NotFound />}/>
+                <Route path='/*' element={<NotFound />}/>
+              </Routes>
+            </main>
 
-          <Footer/>
-      </div>
-    </Router>
+            <Footer/>
+        </div>
+      </Router>
+    </GithubProvider>
   )
 }
 
