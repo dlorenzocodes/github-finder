89bcbc4f897bb9533eacef2ecec5c3778753c84e
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-01-27 09:58:17 -0600

    crated alert context and reducer

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import Home from'./components/pages/Home'
 import About from'./components/pages/About'
 import NotFound from'./components/pages/NotFound'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-            <NavBar/>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+              <NavBar/>
 
-            <main className='container mx-auto px-3 pb-12'>
-              <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/about' element={<About />}/>
-                <Route path='/notfound' element={<NotFound />}/>
-                <Route path='/*' element={<NotFound />}/>
-              </Routes>
-            </main>
+              <main className='container mx-auto px-3 pb-12'>
+                <Routes>
+                  <Route path='/' element={<Home />}/>
+                  <Route path='/about' element={<About />}/>
+                  <Route path='/notfound' element={<NotFound />}/>
+                  <Route path='/*' element={<NotFound />}/>
+                </Routes>
+              </main>
 
-            <Footer/>
-        </div>
-      </Router>
+              <Footer/>
+          </div>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   )
 }
