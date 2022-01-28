dc65456eb598b93372b9e30b45e199dfd548692b
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-01-28 08:57:23 -0600

    finished alert message component

---
+++ src/components/layout/AlertMessage.jsx
@@ -0,0 +1,20 @@
+import {useContext} from 'react'
+import AlertContext from '../../context/alert/AlertContext'
+import {RiErrorWarningFill} from 'react-icons/ri'
+
+
+function AlertMessage() {
+
+    const {alert} = useContext(AlertContext)
+
+    return alert !== null && (
+        <p className='flex items-center mv-4 space-x-2'>
+            {alert.type === 'error' && (
+               <RiErrorWarningFill fill='#FECDD3'/>
+            )}
+            <strong>{alert.msg}</strong>
+        </p>
+    )
+}
+
+export default AlertMessage
